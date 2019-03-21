5102548f01ba8a244ceb6a3421433c23e529ca52
jairos1301 <jairos1301@gmail.com>
2019-03-21 16:35:33 -0500

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {LogoComponent} from './components/logo/logo.component';
-import {HomeComponent} from './components/home/home.component';
-import {NavbarComponent} from './components/navbar/navbar.component';
-import {FooterComponent} from './components/footer/footer.component';
-import {DetalleProductoComponent} from './components/detalle-producto/detalle-producto.component';
-import {ListaProductosComponent} from './components/lista-productos/lista-productos.component';
-import {LoginComponent} from './components/users/login/login.component';
-import {RegisterComponent} from './components/users/register/register.component';
-import {Page404Component} from './components/errors/page404/page404.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LogoComponent } from "./components/logo/logo.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { DetalleProductoComponent } from "./components/detalle-producto/detalle-producto.component";
+import { ListaProductosComponent } from "./components/lista-productos/lista-productos.component";
+import { LoginComponent } from "./components/users/login/login.component";
+import { RegisterComponent } from "./components/users/register/register.component";
+import { Page404Component } from "./components/errors/page404/page404.component";
 
 const routes: Routes = [
-  {path:'Logo', component: LogoComponent},
-  {path:'Home', component: HomeComponent},
-  {path:'Navbar', component: NavbarComponent},
-  {path:'Footer', component: FooterComponent},
-  {path:'DetalleProducto', component: DetalleProductoComponent},
-  {path:'ListaProductos', component: ListaProductosComponent},
-  {path:'Login', component: LoginComponent},
-  {path:'Register', component: RegisterComponent},
-  {path:'Page404', component: Page404Component}
+  { path: "Logo", component: LogoComponent },
+  { path: "Home", component: HomeComponent },
+  { path: "Navbar", component: NavbarComponent },
+  { path: "Footer", component: FooterComponent },
+  { path: "DetalleProducto", component: DetalleProductoComponent },
+  { path: "ListaProductos", component: ListaProductosComponent },
+  { path: "Login", component: LoginComponent },
+  { path: "Register", component: RegisterComponent },
+  { path: "Page404", component: Page404Component }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
